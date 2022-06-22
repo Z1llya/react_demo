20414403602ff49ad4be6c42030ea096595c96b8
Z1llya <zubalii.illya@gmail.com>
2022-06-22 14:28:23 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import Character from "./components/Character";
+
 
 function App() {
   return (
     <div >
-      <Character name={'bart'} img ={'https://wikimultia.org/images/thumb/6/65/Bart_Simpson.png/300px-Bart_Simpson.png'} description ={'fdkdfklgj'}/>
-        <Character name ={'homer'} img ={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'} description ={'fgkgdfkj'}/>
-        <Character name={'marge'} img = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'} description ={'fgkgdfkj'}/>
+
     </div>
   );
 }
