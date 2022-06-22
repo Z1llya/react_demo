990e2fafeefd571ee8fc0c6d6eb648312780aa4c
Z1llya <zubalii.illya@gmail.com>
2022-06-22 14:05:00 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Character from "./components/Character";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Character name={'bart'} img ={'https://wikimultia.org/images/thumb/6/65/Bart_Simpson.png/300px-Bart_Simpson.png'} description ={'fdkdfklgj'}/>
+        <Character name ={"homer"} img ={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'} description ={'fgkgdfkj'}/>
+        <Character name={'marge'} img = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'} description ={'fgkgdfkj'}/>
+
+
     </div>
   );
 }
+
 
 export default App;
